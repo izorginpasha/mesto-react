6c22497654330b67c193327ff8b348e608c781e0
Izorgin Pavel <izorgin.pasha@yandex.ru>
2022-09-25 15:21:08 +0500

    com

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,57 @@
-import React from "react";
-import Header from "./Header.js"; // прошу любить и жаловать компонент header
-import Main from "./Main.js";
-import Footer from "./Footer.js";
-import PopupWithForm from "./PopupWithForm.js";
-import ImagePopup from "./ImagePopup.js";
-import EditProfilePopup from "./EditProfilePopup.js";
-import EditAvatarPopup from "./EditAvatarPopup.js";
-import AddPlacePopup from "./AddPlacePopup.js";
-import "../index.css";
-import { api } from "../utils/Api.js";
+import React from 'react'
+import Header from './Header.js' // прошу любить и жаловать компонент header
+import Main from './Main.js'
+import Footer from './Footer.js'
+import PopupWithForm from './PopupWithForm.js'
+import ImagePopup from './ImagePopup.js'
+import EditProfilePopup from './EditProfilePopup.js'
+import EditAvatarPopup from './EditAvatarPopup.js'
+import AddPlacePopup from './AddPlacePopup.js'
+import '../index.css'
+import { api } from '../utils/Api.js'
 import {
   CurrentUserContext,
   сurrentUser,
-} from "../contexts/CurrentUserContext.js";
+} from '../contexts/CurrentUserContext.js'
 function App(props) {
-  const [currentUsercontext, setCurrentUser] = // стеит данных пользователя
-    React.useState({});
-  const [isEditProfilePopupOpen, setEditProfilePopupOpen] = // стеит открытия попап профиль
-    React.useState(false);
-  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false); // стеит открытия попап карточки создания
-  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false); // стеит открытия попап смена аватара
+  const [currentUsercontext, setCurrentUser] = React.useState({}) // стеит данных пользователя
+  const [isEditProfilePopupOpen, setEditProfilePopupOpen] = React.useState(
+    // стеит открытия попап профиль
+    false,
+  )
+  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false) // стеит открытия попап карточки создания
+  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = React.useState(false) // стеит открытия попап смена аватара
   const [selectedCard, setSelectedCard] = React.useState({
     // стеит открытия попап картинки
     selectedCard: false,
-  });
+  })
   const [card, setCard] = React.useState({
     // стеит открытия попап картинки
     card: {},
-  });
+  })
   const [cards, setCards] = React.useState({
     //массив карточек
     cards: [],
-  });
+  })
 
-  const сurrentUser = React.useContext(CurrentUserContext);
+  const сurrentUser = React.useContext(CurrentUserContext)
 
   function handleCardLike(card) {
     // обрабочик лаика
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some((i) => i._id === currentUsercontext._id);
+    const isLiked = card.likes.some((i) => i._id === currentUsercontext._id)
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api
       .like(card._id, !isLiked)
       .then((newCard) => {
         const isCards = cards.cards.map((c) =>
-          c._id === card._id ? newCard : c
-        );
-        setCards({ cards: isCards });
+          c._id === card._id ? newCard : c,
+        )
+        setCards({ cards: isCards })
       })
       .catch((err) => {
-        console.log(err); // выведем ошибку в консоль
-      });
+        console.log(err) // выведем ошибку в консоль
+      })
   }
   function handleCardDelete(card) {
     api
@@ -59,16 +60,16 @@ function App(props) {
         setCards({
           cards: cards.cards.filter(function (n) {
             if (n._id === card._id) {
-              return false;
+              return false
             }
 
-            return true;
+            return true
           }),
-        });
+        })
       })
       .catch((err) => {
-        console.log(err); // выведем ошибку в консоль
-      });
+        console.log(err) // выведем ошибку в консоль
+      })
   }
   React.useEffect(() => {
     function userCards() {
@@ -78,14 +79,14 @@ function App(props) {
         .then((result) => {
           setCards({
             cards: result,
-          });
+          })
         })
         .catch((err) => {
-          console.log(err); // выведем ошибку в консоль
-        });
+          console.log(err) // выведем ошибку в консоль
+        })
     }
-    userCards();
-  }, []);
+    userCards()
+  }, [])
 
   React.useEffect(() => {
     function iscurrentUser() {
@@ -93,74 +94,74 @@ function App(props) {
       api
         .getUser()
         .then((result) => {
-          setCurrentUser(result);
-          Object.assign(currentUsercontext, result);
+          setCurrentUser(result)
+          Object.assign(currentUsercontext, result)
         })
         .catch((err) => {
-          console.log(err); // выведем ошибку в консоль
-        });
+          console.log(err) // выведем ошибку в консоль
+        })
     }
-    iscurrentUser();
-  }, []);
+    iscurrentUser()
+  }, [])
   function handleCardClick(card) {
     // обрабочик клика клика по каринке
 
-    setCard({ card: card });
+    setCard({ card: card })
     setSelectedCard({
       selectedCard: true,
-    });
+    })
   }
   function handleEditAvatarClick() {
     // обрабочик смены аватара
-    setEditAvatarPopupOpen(true);
+    setEditAvatarPopupOpen(true)
   }
   function handleEditProfileClick() {
     // обрабочик смены данных профиля
-    setEditProfilePopupOpen(true);
+    setEditProfilePopupOpen(true)
   }
   function handleAddPlaceClick() {
     // обрабочик добавления карточки
-    setAddPlacePopupOpen(true);
+    setAddPlacePopupOpen(true)
   }
   function closeAllPopups() {
     // обрабочик закрытия попап
 
-    setEditProfilePopupOpen(false);
-    setAddPlacePopupOpen(false);
-    setEditAvatarPopupOpen(false);
-    setSelectedCard({ selectedCard: false });
+    setEditProfilePopupOpen(false)
+    setAddPlacePopupOpen(false)
+    setEditAvatarPopupOpen(false)
+    setSelectedCard({ selectedCard: false })
   }
   function handleUpdateUser({ name, about }) {
     // обрабочик добавления данных профмля
-    //setTextButtonSave();
+
     api
       .setUserProfile(name, about)
       .then((result) => {
-        setCurrentUser(result);
-        Object.assign(currentUsercontext, result); // добавляем результат запроса на страницу
-        setEditProfilePopupOpen(false); //закрываем окно
+        setCurrentUser(result)
+        Object.assign(currentUsercontext, result) // добавляем результат запроса на страницу
+        setEditProfilePopupOpen(false) //закрываем окно
       })
       .catch((err) => {
-        console.log(err); // выведем ошибку в консоль
+        console.log(err) // выведем ошибку в консоль
       })
       .finally(() => {
         // popupProfile.buttonSpan.textContent = "Сохранить";
-      });
+      })
   }
   function handleUpdateAvatar({ avatar }) {
     // обновление аватара
     api
       .setUserAvatar(avatar)
       .then((result) => {
-        Object.assign(currentUsercontext, result); // добавляем результат запроса на страницу
-        setEditAvatarPopupOpen(false); //закрываем окно
+        Object.assign(currentUsercontext, result) // добавляем результат запроса на страницу
+        setEditAvatarPopupOpen(false) //закрываем окно
       })
       .catch((err) => {
-        console.log(err); // выведем ошибку в консоль
+        console.log(err) // выведем ошибку в консоль
       })
       .finally(() => {
         //popupNewAvatar.buttonSpan.textContent="Сохранить";
-      });
+      })
   }
   function handleAddPlaceSubmit({ popupName, popupLink }) {
     // функция обрабочик кнопки создать карточку
@@ -168,15 +169,15 @@ function App(props) {
     api
       .setCard(popupName, popupLink)
       .then((result) => {
-        setCards({ cards: [result, ...cards.cards] });
-        setAddPlacePopupOpen(false);
+        setCards({ cards: [result, ...cards.cards] })
+        setAddPlacePopupOpen(false)
       })
       .catch((err) => {
-        console.log(err); // выведем ошибку в консоль
+        console.log(err) // выведем ошибку в консоль
       })
       .finally(() => {
         // popupNewMesto.buttonSpan.textContent = "Сохранить";
-      });
+      })
   }
   return (
     <CurrentUserContext.Provider value={currentUsercontext}>
@@ -214,7 +215,7 @@ function App(props) {
           <PopupWithForm
             name="#popupDelCard"
             title="Вы уверены?"
-            children={""}
+            children={''}
             isOpen={false}
             onClose={closeAllPopups}
           />
@@ -226,6 +227,6 @@ function App(props) {
         </div>
       </div>
     </CurrentUserContext.Provider>
-  );
+  )
 }
-export default App;
+export default App
